a748dc74677d721c249350c00dae610ec1e832ac
Sylviane Farooq <sylvianepinto@hotmail.fr>
2024-02-05 12:02:52 +0100

    homepage hero corrections

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="main">
       <article className="main-info-container">
         <h1>One app for all things fitness, wellness & beauty</h1>
-        <p>
+        <p className="intro">
           ClassPass gives you worldwide access to thousands of top-rated gyms,
           fitness studios, salons and spas.
         </p>
@@ -13,7 +13,7 @@ export default function Home() {
           <button className="primary">Try for free</button>
           <button className="secondary">Tell me more about ClassPass</button>
         </div>
-        <Link href={"/search"}>Browse classes & appointments</Link>
+        <Link className="browse" href={"/search"}>Browse classes & appointments</Link>
         <p className="disclaimer">Offer is available to new trialers only. Spa and salons appointments are available after trial.</p>
       </article>
     </div>
